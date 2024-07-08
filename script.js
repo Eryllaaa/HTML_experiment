8ad3ce4f8b46d839d4390e48ba1cf16331327b5c
Eryllaaa <allyre@gmail.com>
2024-07-08 19:58:29 +0200

    fixed

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const data = { username: 'johndoe', password: 'secretpassword' };
 
 token = {};
 
-fetch(POST_URL, { method: 'POST', headers: { 'Content-Type': 'x-www-form-urlencoded' }, body: 'client_id=cvs7ivnkujeg4vn4atbnayc4mjhn6e&client_secret=yqolzjtykklwwi8a74a2yhdoecvh04&grant_type=client_credentials'})
+fetch(POST_URL, { method: 'POST', headers: { 'Content-Type': 'x-www-form-urlencoded' }, body: 'client_id=cvs7ivnkujeg4vn4atbnayc4mjhn6e&client_secret=3l6jsxbzylavaz5xdg6dn85hl3rm9m&grant_type=client_credentials' })
     .then(response => response.json())
     .then(data => {
         //console.log('Success:', data);
-        token = data;
+        //token = data;
         changeText("statusDisplay", "status: success");
-        ChangeText("token", "access token: " + toString(data));
+        //ChangeText("token", toString(data));
     })
     .catch((error) => {
         //console.error('Error:', error);
