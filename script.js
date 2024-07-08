76c4c2ca6342b9cd75bc1906ed2a131f4857c7cb
Eryllaaa <allyre@gmail.com>
2024-07-08 19:09:42 +0200

    changes

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const url = 'https://id.twitch.tv/oauth2/token';
+const POST_URL = 'https://id.twitch.tv/oauth2/token';
+const GET_URL = 'https://id.twitch.tv/oauth2/token';
 const data = { username: 'johndoe', password: 'secretpassword' };
 
 token = {}
 
-fetch(url, { method: 'POST', headers: {'Content-Type': 'x-www-form-urlencoded'}, body: 'client_id=cvs7ivnkujeg4vn4atbnayc4mjhn6e&client_secret=3l6jsxbzylavaz5xdg6dn85hl3rm9m&grant_type=client_credentials'})
+fetch(POST_URL, { method: 'POST', headers: {'Content-Type': 'x-www-form-urlencoded'}, body: 'client_id=cvs7ivnkujeg4vn4atbnayc4mjhn6e&client_secret=3l6jsxbzylavaz5xdg6dn85hl3rm9m&grant_type=client_credentials'})
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
         token = data;
-        DisplayToken("success");
+        changeText(statusDisplay, "success");
     })
     .catch((error) => {
         console.error('Error:', error);
-        DisplayToken("failed");
+        changeText(statusDisplay, "failure");
     });
 
-function changeText() {
-    const newText = "New text content";
-    document.getElementById("text").textContent = token[access_token];
+//fetch(GET_URL, {})
+
+
+function changeText(id, text) {
+    document.getElementById(toString(id)).textContent = toString(text);
 }
 
 function DisplayToken(token) {
