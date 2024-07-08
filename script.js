a4dcf8fdc1b4e5b97c342f95661d46a70a53b0fd
Eryllaaa <allyre@gmail.com>
2024-07-08 19:04:48 +0200

    changes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const url = 'https://id.twitch.tv/oauth2/token';
 const data = { username: 'johndoe', password: 'secretpassword' };
 
+token = {}
+
 fetch(url, { method: 'POST', headers: {'Content-Type': 'x-www-form-urlencoded'}, body: 'client_id=cvs7ivnkujeg4vn4atbnayc4mjhn6e&client_secret=3l6jsxbzylavaz5xdg6dn85hl3rm9m&grant_type=client_credentials'})
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
+        token = data;
         DisplayToken("success");
     })
     .catch((error) => {
@@ -14,7 +17,7 @@ fetch(url, { method: 'POST', headers: {'Content-Type': 'x-www-form-urlencoded'},
 
 function changeText() {
     const newText = "New text content";
-    document.getElementById("text").textContent = newText;
+    document.getElementById("text").textContent = token[access_token];
 }
 
 function DisplayToken(token) {
